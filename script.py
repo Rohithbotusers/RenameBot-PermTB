3b0a01b813554bb38a9ab1d001169158be590fc6
fazinek <76585579+fazinek@users.noreply.github.com>
2021-05-05 10:46:11 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,25 @@
 class script(object):
-    START_TEXT = """ A Simple And Fastest File Renamer Bot With Permanent Thumbnail support!💯
+    START_TEXT = """ Hey Dear {},
+<b>I Am Simple Tg File Renamer Bot With Permanent Thumbnail Support & Auto Detection Of Files.
 
-<b>Send me any Telegram file and choose appropriate option! 
-For Know More Hit /help.</b>"""
+Hit /help For See A Magic 😉.</b>"""
 
 
     RENAME_403_ERR = "What Are You Doing? You are Banned"
-    UPGRADE_TEXT = "<b>CONTACT</b> @Ek_comme_nt_bot"
+    UPGRADE_TEXT = "<b>CONTACT @Feedback_ek_bot</b>"
     DOWNLOAD_START = "Give Me Some Time 🙏..."
-    UPLOAD_START = "Starting to upload 😝..."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thanks you for Using Me. @ekbotz_update.**"
-    SAVED_THUMB = "Thumbnail Saved as permanent ✅. This Is Permanent"
+    UPLOAD_START = "Uploading Started To Tg 😝..."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thanks for Using Me. @ekbotz_update.**"
+    SAVED_THUMB = "Permanent Thumbnail Saved Successfully ✅. "
     DEL_THUMB = "Thumbnail cleared succesfully!"
     NO_THUMB = "No thumbnails found!😐"
-    SAVED_RECVD_DOC_FILE = "File Downloaded Successfully 😎"
+    SAVED_RECVD_DOC_FILE = "Your File Downloaded Successfully 😎"
     CUSTOM_CAPTION_UL_FILE = " "
-    HELP_USER = """Hey,
- It's not that complicated😎
+    HELP_USER = """Hello
+ <b><u>It's Simple To Use. Just Look Following Things To Get An Idea About Me.</u></b>
     
-<b>1. Send me any Telegram File.
-2. Choose appropriate option.
-   Enjoy😍.</b>"""
+<b>• Send me any Telegram File.
+• Wait Few Seconds.
+• Choose appropriate option.
+• Give New Name Without Extension.</b>"""
 
